7db18fa3e2f7deb4b44db64c49f68e4b533a3a27
Eduardo Patrocinio <epatro@gmail.com>
2019-07-18 15:21:50 -0400

    Create prepare bastion

--- OpenShift/prepare_bastion.sh
+++ OpenShift/prepare_bastion.sh
@@ -15,9 +15,15 @@ function copy_key {
 function install_subscriptions {
   subscription-manager repos --disable="*"
   subscription-manager refresh
-  subscription-manager attach --pool=$POOL_ID  
+  subscription-manager attach --pool=$POOL_ID
+}
+
+function enable_yum_repos {
+  subscription-manager repos --enable="rhel-7-server-rpms" --enable="rhel-7-server-extras-rpms" --enable="rhel-7-server-ose-3.11-rpms" --enable="rhel-7-server-ansible-2.6-rpms" --enable="rh-gluster-3-client-for-rhel-7-server-rpms"
+yum update -y
 }
 
 #create_ssh_key
 #copy_key
-install_subscriptions
+#install_subscriptions
+enable_yum_repos
