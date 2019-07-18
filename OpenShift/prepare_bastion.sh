f17619f2522988120c58d11e3b9938b9be4eab12
Eduardo Patrocinio <epatro@gmail.com>
2019-07-18 15:16:36 -0400

    Create prepare bastion

--- OpenShift/prepare_bastion.sh
+++ OpenShift/prepare_bastion.sh
@@ -3,7 +3,7 @@ function create_ssh_key {
 }
 
 function copy_key {
-  for m in master worker1 worker2 worker3 storage storage2 storage3 infra
+  for m in master worker1 worker2 worker3 storage1 storage2 storage3 infra
   do
     echo Copying to $m
     ssh-copy-id -i ~/.ssh/id_rsa.pub root@$m.patrocinio.org
