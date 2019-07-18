062453b97c0e381bca0921c1314340437b5225f5
Eduardo Patrocinio <epatro@gmail.com>
2019-07-18 10:01:18 -0400

    Create OpenShift install

--- OpenShift/install_open_shift.sh
+++ OpenShift/install_open_shift.sh
@@ -14,7 +14,16 @@ function disable_yum_repos {
   subscription-manager repos --disable="*"
 }
 
+function enable_OpenShift_repo {
+  subscription-manager repos \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-ose-3.11-rpms" \
+    --enable="rhel-7-server-ansible-2.6-rpms"
+}
+
 #register_host
 #find_pool_ID
 #attach_pool_ID
-disable_yum_repos
+#disable_yum_repos
+enable_OpenShift_repo
