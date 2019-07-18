966eb2cdc6f37863327952e580054c5df3f34f5d
Eduardo Patrocinio <epatro@gmail.com>
2019-07-18 16:34:31 -0400

    Create deploy openshift

--- OpenShift/deploy_openshift.sh
+++ OpenShift/deploy_openshift.sh
@@ -1,2 +1,12 @@
-sed "s/REDHAT_USER/$REDHAT_USER/g" < hosts > /tmp/hosts
-sed "s/REDHAT_PASSWORD/$REDHAT_PASSWORD/g" < /tmp/hosts > /etc/ansible/hosts 
+function configure_credentials {
+  sed "s/REDHAT_USER/$REDHAT_USER/g" < hosts > /tmp/hosts
+  sed "s/REDHAT_PASSWORD/$REDHAT_PASSWORD/g" < /tmp/hosts > /etc/ansible/hosts
+}
+
+function install_prereqs {
+  cd /usr/share/ansible/openshift-ansible
+  ansible-playbook playbooks/prerequisites.yml
+}
+
+#configure_credentials
+install_prereqs
