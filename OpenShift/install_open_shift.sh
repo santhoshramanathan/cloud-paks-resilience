bf0b5d976d341a717a426c9cbc0a15e927944a41
Eduardo Patrocinio <epatro@gmail.com>
2019-07-18 09:59:15 -0400

    Create OpenShift install

--- OpenShift/install_open_shift.sh
+++ OpenShift/install_open_shift.sh
@@ -6,5 +6,10 @@ function find_pool_ID {
   POOL_ID=2c9a01226880d2d50168811685400be5
 }
 
-register_host
+function attach_pool_ID {
+  subscription-manager attach --pool=$POOL_ID
+}
+
+#register_host
 find_pool_ID
+attach_pool_ID
