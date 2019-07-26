e0f09e0870f708d9b22f492f4c57331bced4807d
Eduardo Patrocinio <epatro@gmail.com>
2019-07-26 15:00:25 -0400

    Create install cp4i

--- Integration/install_cp4i.sh
+++ Integration/install_cp4i.sh
@@ -1,8 +1,26 @@
 PROJECT=cp4i
 SCC=ibm-privileged-scc
+INSTALL_DIR=/root/Integration/install/installer_files/cluster/icipcontent
+APIC=IBM-API-Connect-Enterprise-for-IBM-Cloud-Integration-Platform-1.0.0.tgz
 
-echo Creating project...
-oc new-project $PROJECT
+function createProject {
+  echo Creating project...
+  oc new-project $PROJECT
+}
 
-echo Updating Security Context...
-oc adm policy add-scc-to-user $SCC system:serviceaccounts:$PROJECT
+function updateSCC {
+  echo Updating Security Context...
+  oc adm policy add-scc-to-user $SCC system:serviceaccounts:$PROJECT
+}
+
+function unpackAPIC {
+  echo Unpacking API Connect...
+  cd $INSTALL_DIR
+  tar xzvf $APIC
+}
+
+
+
+# createProject
+# updateSCC
+unpackAPIC
