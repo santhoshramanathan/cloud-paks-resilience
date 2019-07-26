8bde1ba510aa341e4b8c00afb413d8a6a584b829
Eduardo Patrocinio <epatro@gmail.com>
2019-07-26 15:03:47 -0400

    Create install cp4i

--- Integration/install_cp4i.sh
+++ Integration/install_cp4i.sh
@@ -2,6 +2,7 @@ PROJECT=cp4i
 SCC=ibm-privileged-scc
 INSTALL_DIR=/root/Integration/install/installer_files/cluster/icipcontent
 APIC=IBM-API-Connect-Enterprise-for-IBM-Cloud-Integration-Platform-1.0.0.tgz
+WORK_DIR=/root/work
 
 function createProject {
   echo Creating project...
@@ -14,9 +15,12 @@ function updateSCC {
 }
 
 function unpackAPIC {
+  echo Creating work directory...
+  mkdir -p $WORK_DIR
+  cd $WORK_DIR
+
   echo Unpacking API Connect...
-  cd $INSTALL_DIR
-  tar xzvf $APIC
+  tar xzvf $INSTALL_DIR/$APIC
 }
 
 
