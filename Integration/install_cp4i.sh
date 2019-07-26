b92ddf2d2c6c013d664546fe5fe4f3f22b143398
Eduardo Patrocinio <epatro@gmail.com>
2019-07-26 14:53:24 -0400

    Create install cp4i

--- Integration/install_cp4i.sh
+++ Integration/install_cp4i.sh
@@ -1,7 +1,8 @@
 PROJECT=cp4i
+SCC=ibm-privileged-scc
 
 echo Creating project...
 oc new-project $PROJECT
 
 echo Updating Security Context...
-oc adm policy add-scc-to-user ibm-restricted-scc system:serviceaccounts:$PROJECT
+oc adm policy add-scc-to-user $SCC system:serviceaccounts:$PROJECT
