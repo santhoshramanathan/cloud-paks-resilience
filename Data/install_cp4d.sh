8d106b9541e4ff989896126d8f8cbff131827ada
Eduardo Patrocinio <epatro@gmail.com>
2019-07-27 10:24:42 -0400

    Create install cp4d

---
+++ Data/install_cp4d.sh
@@ -0,0 +1,9 @@
+export IMAGE_DIR=/root/Data/images
+export WORK_DIR=/root/work_cp4d
+
+function download {
+  chmod +x $IMAGE_DIR/ICP4D_ENT_Req_ICP_x86_V2.1.0.1.bin
+
+  cd $WORK_DIR
+  $IMAGE_DIR/ICP4D_ENT_Req_ICP_x86_V2.1.0.1.bin
+}
