703f0e4fb696afe920018d265295f11809d0e191
patrocinio <epatro@gmail.com>
2019-08-19 16:10:24 +0100

    Log in to ICP

--- Integration/deploy_apic.sh
+++ Integration/deploy_apic.sh
@@ -3,7 +3,7 @@ SCC=ibm-anyuid-hostpath-scc
 IMAGE_SECRET=myregistrykey
 CLUSTER_DOMAIN=docker-registry-default.patrocinio-fa9ee67c9ab6a7791435450358e564cc-0001.us-east.containers.appdomain.cloud
 ICP_PASSWORD=passw0rd
-
+CLUSTER_IP=https://icp-console.patrocinio-fa9ee67c9ab6a7791435450358e564cc-0001.us-east.containers.appdomain.cloud
 
 function createProject {
   oc new-project $PROJECT
@@ -18,9 +18,13 @@ function createImagePullSecret {
   oc create secret docker-registry $IMAGE_SECRET \
     --docker-server=$CLUSTER_DOMAIN:8500 --docker-username=admin \
     --docker-password=$ICP_PASSWORD --docker-email=patro@patro.org
+}
 
+function login {
+  cloudctl login -a https://$CLUSTER_IP:8443 --skip-ssl-validation
 }
 
 #createProject
 #associateSCC
-createImagePullSecret
+#createImagePullSecret
+login
