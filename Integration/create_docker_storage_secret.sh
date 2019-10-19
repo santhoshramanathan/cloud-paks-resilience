21ea93395138fbcf2e23e016c6730611cb3e4cd6
patrocinio <epatro@gmail.com>
2019-10-19 11:53:38 -0400

    Create create_docker_storage_secret.sh

---
+++ Integration/create_docker_storage_secret.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry apic-docker-registry \
+   --docker-server=http://docker-registry.default.svc:5000/v1/ \
+   --docker-username=$(oc whoami) --docker-password=$(oc whoami -t) \
+  --docker-email=me@my.com
+  
