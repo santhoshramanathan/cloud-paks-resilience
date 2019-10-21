93dadf8c0e0b36ac3ccb68f46f7fadb295ebd1ba
Eduardo Patrocinio <epatro@gmail.com>
2019-10-21 08:14:10 -0400

    Recycle docker registry secret

--- Integration/create_docker_storage_secret.sh
+++ Integration/create_docker_storage_secret.sh
@@ -1,4 +1,10 @@
-kubectl create secret docker-registry apic-docker-registry \
+SECRET=apic-docker-registry
+
+echo Deleting secret $SECRET
+kubectl delete secret $SECRET
+
+echo Creating secret $SECRET
+kubectl create secret docker-registry $SECRET \
    --docker-server=http://docker-registry.default.svc:5000/v1/ \
    --docker-username=$(oc whoami) --docker-password=$(oc whoami -t) \
   --docker-email=me@my.com
