e8ec7048c48573a32e2736bdfe5213b333b4bceb
Eduardo Patrocinio <epatro@gmail.com>
2019-10-23 17:48:29 -0400

    Install OC

--- Integration/unzip_image.sh
+++ Integration/unzip_image.sh
@@ -1,6 +1,7 @@
 IMAGE_DIR=/images/Integration/3.2
 IMAGE=ibm-cloud-pak-for-integration-x86_64-2019.3.2.1-for-OpenShift.tar.gz
 WORK_DIR=/root/work_cp4i
+OPENSHIFT_CLIENT=openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
 
 function unzipImage {
   echo Unzipping image...
@@ -8,7 +9,8 @@ function unzipImage {
 }
 
 function installOC {
-  wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+  wget https://github.com/openshift/origin/releases/download/v3.11.0/$OPENSHIFT_CLIENT
+  tar xvf $OPENSHIFT_CLIENT
 }
 
 function listNodes {
