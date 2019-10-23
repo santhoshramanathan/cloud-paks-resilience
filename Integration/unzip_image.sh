144e134c1cbf1c2f6b4e0b02fbb95762bfc8cef5
Eduardo Patrocinio <epatro@gmail.com>
2019-10-23 17:47:26 -0400

    Install OC

--- Integration/unzip_image.sh
+++ Integration/unzip_image.sh
@@ -8,7 +8,7 @@ function unzipImage {
 }
 
 function installOC {
-  wget v3.11.0-0cbc58b-linux-64bit.tar.gz
+  wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
 }
 
 function listNodes {
