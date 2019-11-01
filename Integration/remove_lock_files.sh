710e8650e92294dc952dc65ebb79536643b6bec3
Eduardo Patrocinio <epatro@gmail.com>
2019-11-01 17:29:52 -0400

    Create remove lock files

--- Integration/remove_lock_files.sh
+++ Integration/remove_lock_files.sh
@@ -1,7 +1,7 @@
 WORK_DIR=/root/work_cp4i
 INSTALLER_FILES_DIR=$WORK_DIR/installer_files/cluster
 
-cd $WORK_DIR/$INSTALLER_FILES_DIR
+cd $INSTALLER_FILES_DIR
 rm .openshift-images-uploaded-3.2.0.1907.lock
 cd icp4icontent
 rm -f *.lock
