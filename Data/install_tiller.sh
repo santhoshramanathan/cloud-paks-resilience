55560ac9138b144fa4afd37bbd68becb9872e6af
Eduardo Patrocinio <epatro@gmail.com>
2019-11-07 09:16:34 -0500

    Enhance install tiller

--- Data/install_tiller.sh
+++ Data/install_tiller.sh
@@ -4,11 +4,17 @@ function createProject {
     oc new-project $TILLER_NAMESPACE
 }
 
-function installHelm {
+function installHelmClient {
   curl -s https://storage.googleapis.com/kubernetes-helm/helm-v2.9.1-linux-amd64.tar.gz | tar xz
   cd linux-amd64
   ./helm init --client-only
 }
 
+function installHelmServer {
+oc process -f https://github.com/openshift/origin/raw/master/examples/helm/tiller-template.yaml \
+  -p TILLER_NAMESPACE="${TILLER_NAMESPACE}" -p HELM_VERSION=v2.9.0 | oc create -f -
+}
+
 #createProject
-installHelm
+#installHelmClient
+installHelmServer
