bfbac8c30519a8794f66f9f86bffe95eef557b76
patrocinio <epatro@gmail.com>
2019-11-08 13:47:31 -0500

    Update deploy apic

--- Integration/deploy_apic.sh
+++ Integration/deploy_apic.sh
@@ -1,13 +1,19 @@
-#NAME=$1
+NAME=my-apic-v7
 PROJECT=apic
 SCC=ibm-anyuid-hostpath-scc
 ACCOUNT=my-apic-v4-ibm-apiconnect-icp4i-prod
+WORK_DIR=/root/work_cp4i
 
-#echo Deploying $NAME
-#helm install ibm-apiconnect-icp4i-prod-1.0.2 --name $NAME --values apic.yaml
+function deployHelm {
+  cd $WORK_DIR
+  echo Deploying $NAME
+  helm install ibm-apiconnect-icp4i-prod --name $NAME \
+    --tls --debug
+}
 
 function associateSCC {
   oc adm policy add-scc-to-user $SCC system:serviceaccount:$PROJECT:$ACCOUNT
 }
 
-associateSCC
+#associateSCC
+deployHelm
