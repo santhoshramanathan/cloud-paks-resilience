c1f068d93664af08378db27d506803c3ccef9d86
patrocinio <epatro@gmail.com>
2019-11-08 13:25:04 -0500

    Add download helm chart

--- Integration/download_helm_chart.sh
+++ Integration/download_helm_chart.sh
@@ -1,1 +1,5 @@
-curl -k -L https://github.com/IBM/charts/blob/master/repo/entitled/ibm-apiconnect-icp4i-prod-1.0.3.tgz > ibm-apiconnect-icp4i-prod-1.0.3.tgz 
+WORK_DIR=/root/work_cp4i
+
+cd $WORK_DIR
+echo Downloading Helm chart
+curl -k -L https://github.com/IBM/charts/blob/master/repo/entitled/ibm-apiconnect-icp4i-prod-1.0.3.tgz > ibm-apiconnect-icp4i-prod-1.0.3.tgz
