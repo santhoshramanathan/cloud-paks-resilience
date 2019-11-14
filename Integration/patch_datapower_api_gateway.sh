f4da0a611fcecfed1eb1a5b37b61ebaf8eaa9773
Eduardo Patrocinio <epatro@gmail.com>
2019-11-14 09:20:17 -0500

    Add patch datapower api gateway

--- Integration/patch_datapower_api_gateway.sh
+++ Integration/patch_datapower_api_gateway.sh
@@ -1,5 +1,12 @@
+function configureAccessToRegistry {
+  echo Setting port forward
+  kubectl port-forward svc/docker-registry 5000 -n default &
+  sleep 2
+}
+
 function loginToDocker {
   docker login -p M1383K6UdWvEOPH3LAMpIwTZ8XDNl__MWlwZH3XT0yc -u unused docker-registry.default.svc:5000
 }
 
+configureAccessToRegistry
 loginToDocker
