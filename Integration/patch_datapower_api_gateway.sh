c15c83488e368e2cde383326c8b449486f52f33d
Eduardo Patrocinio <epatro@gmail.com>
2019-11-14 09:23:43 -0500

    Work on patch datapower api gateway

--- Integration/patch_datapower_api_gateway.sh
+++ Integration/patch_datapower_api_gateway.sh
@@ -8,5 +8,10 @@ function loginToDocker {
   docker login -p M1383K6UdWvEOPH3LAMpIwTZ8XDNl__MWlwZH3XT0yc -u unused docker-registry.default.svc:5000
 }
 
-configureAccessToRegistry
-loginToDocker
+function pullImage {
+  docker-registry.default.svc:5000/apic/datapower-api-gateway:2018.4.1.7-312001-release-prod
+}
+
+#configureAccessToRegistry
+#loginToDocker
+pullImage
