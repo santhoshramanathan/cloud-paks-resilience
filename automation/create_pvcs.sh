432bee7a34cee74ceaef8130c040dd0331feb282
patrocinio <epatro@gmail.com>
2020-01-22 12:18:18 -0500

    Create PVs and PVCs for automation

--- automation/create_pvcs.sh
+++ automation/create_pvcs.sh
@@ -1,39 +1,51 @@
-#oc delete -f cpe-cfgstore-pvc.yaml
+oc delete -f cpe-cfgstore-pvc.yaml
 oc apply -f cpe-cfgstore-pv.yaml
 oc apply -f cpe-cfgstore-pvc.yaml
 
-#oc delete -f cpe-logstore-pvc.yaml
+oc delete -f cpe-logstore-pvc.yaml
 oc apply -f cpe-logstore-pv.yaml
 oc apply -f cpe-logstore-pvc.yaml
 
+oc delete -f cpe-filestore-pvc.yaml
 oc apply -f cpe-filestore-pv.yaml
 oc apply -f cpe-filestore-pvc.yaml
 
+oc delete -f cpe-bootstrap-pvc.yaml
 oc apply -f cpe-bootstrap-pv.yaml
 oc apply -f cpe-bootstrap-pvc.yaml
 
+oc delete -f cpe-textext-pvc.yaml
 oc apply -f cpe-textext-pv.yaml
 oc apply -f cpe-textext-pvc.yaml
 
+oc delete -f cpe-icmrules-pvc.yaml
 oc apply -f cpe-icmrules-pv.yaml
 oc apply -f cpe-icmrules-pvc.yaml
 
+oc delete -f css-cfgstore-pvc.yaml
 oc apply -f css-cfgstore-pv.yaml
 oc apply -f css-cfgstore-pvc.yaml
 
+oc delete -f css-tempstore-pvc.yaml
 oc apply -f css-tempstore-pv.yaml
 oc apply -f css-tempstore-pvc.yaml
 
+oc delete -f css-logstore-pvc.yaml
 oc apply -f css-logstore-pv.yaml
 oc apply -f css-logstore-pvc.yaml
 
+oc delete -f css-indexstore-pvc.yaml
 oc apply -f css-indexstore-pv.yaml
 oc apply -f css-indexstore-pvc.yaml
 
+oc delete -f css-icp-customstore-pvc.yaml
 oc apply -f css-icp-customstore-pv.yaml
 oc apply -f css-icp-customstore-pvc.yaml
 
+oc delete -f cmis-cfgstore-pvc.yaml
 oc apply -f cmis-cfgstore-pv.yaml
 oc apply -f cmis-cfgstore-pvc.yaml
 
+oc delete -f cmis-logstore-pvc.yaml
+oc apply -f cmis-logstore-pv.yaml
 oc apply -f cmis-logstore-pvc.yaml
